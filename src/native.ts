c44aa80370ba5443bac56b1224fb5b104383403a
Nike <nkaz2003@gmail.com>
2022-06-11 14:07:25 +0300

    Native realization

---
+++ src/native.ts
@@ -0,0 +1,64 @@
+import { spawn } from 'child_process';
+import inquirer from 'inquirer';
+
+(async function () {
+	const { result: fullPath } = await inquirer.prompt([
+		{
+			type: 'input',
+			name: 'result',
+			message: 'Путь до исходного файла',
+		},
+	]);
+	const { result: width } = await inquirer.prompt([
+		{
+			type: 'number',
+			name: 'result',
+			message: 'Ширина',
+		},
+	]);
+	const { result: height } = await inquirer.prompt([
+		{
+			type: 'number',
+			name: 'result',
+			message: 'Высота',
+		},
+	]);
+	const { result: name } = await inquirer.prompt([
+		{
+			type: 'input',
+			name: 'result',
+			message: 'Название',
+		},
+	]);
+	const { result: ext } = await inquirer.prompt([
+		{
+			type: 'input',
+			name: 'result',
+			message: 'Расширение',
+		},
+	]);
+
+	const path: string = fullPath.match(/(.+)\\.+/)[1];
+
+	const res = spawn('ffmpeg', [
+		'-i',
+		fullPath,
+		'-c:v',
+		'libx264',
+		'-s',
+		`${width}x${height}`,
+		`${path}\\${name}.${ext}`,
+	]);
+
+	res.stdout.on('data', (data: any) => {
+		console.log(data.toString());
+	});
+
+	res.stderr.on('data', (data: any) => {
+		console.log(data.toString());
+	});
+
+	res.on('close', () => {
+		console.log('Done');
+	});
+})();
