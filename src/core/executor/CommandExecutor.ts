d48c233124b50ece1add0842a0eb0ed0e97363ba
Nike <nkaz2003@gmail.com>
2022-07-05 09:16:12 +0300

    Command executor

---
+++ src/core/executor/CommandExecutor.ts
@@ -0,0 +1,26 @@
+import { ChildProcessWithoutNullStreams, spawn } from 'child_process';
+import { IStreamLogger } from '../handlers/streamLogger.types';
+import ICommandExecutor from './CommandExecutor.types';
+
+export abstract class CommandExecutor<Input> {
+	constructor(private logger: IStreamLogger) {}
+
+	public async execute() {
+		const input = await this.prompt();
+		const cmd = this.build(input);
+		const stream = this.spawn(cmd);
+		this.processStream(stream, this.logger);
+	}
+
+	protected abstract prompt(): Promise<Input>;
+	protected abstract build(input: Input): ICommandExecutor;
+	protected abstract spawn(
+		cmd: ICommandExecutor
+	): ChildProcessWithoutNullStreams;
+	protected abstract processStream(
+		stream: ChildProcessWithoutNullStreams,
+		logger: IStreamLogger
+	): void;
+}
+
+spawn('ffmpeg');
---
+++ src/core/executor/CommandExecutor.types.ts
@@ -0,0 +1,4 @@
+export default interface ICommandExecutor {
+	command: string;
+	args: string[];
+}
